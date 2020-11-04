3150c47d4f40fb308abf8907412aca83fe4b9bb1
Rumpa Giri <giri.rumpa2017@gmail.com>
2020-11-04 09:28:21 -0800

    First scraping experiment using facebook career URL

---
+++ webscraper_demo.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_job_listing(url):
+    req = requests.get(url)
+
+    soup = BeautifulSoup(req.text, 'lxml')
+
+    links = soup.find('div', {'class': '_8tk7'}).findAll('a')
+    print(len(links))
+
+    for link in links:
+        job_details = dict()
+        job_details['name'] = link.text
+        job_details['href'] = link['href']
+        print(job_details)
+
+get_job_listing('https://www.facebook.com/careers/jobs/')
