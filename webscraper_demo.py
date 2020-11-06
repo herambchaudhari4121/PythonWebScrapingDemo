4c66c12639c5269a889da551bba30a44c41a86a9
Rumpa Giri <giri.rumpa2017@gmail.com>
2020-11-06 12:53:21 -0800

    Add minimum qualifications and preferred qualifications extraction code sample.

--- webscraper_demo.py
+++ webscraper_demo.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+
+# TODO Potential room for improvmenet would be to save the base URL of
+# the company and append the careers relative URL if that is the trend across
+# organizations.
 
 def get_jobs():
     return get_job_listing('https://www.facebook.com/careers/jobs/')
@@ -20,4 +25,34 @@ def get_job_listing(url):
 
     return job_details
 
-get_job_listing('https://www.facebook.com/careers/jobs/')
+
+# For the POC emo purposes, I am hardcoding the home base URL
+def make_full_facebook_job_url(url):
+    return 'https://www.facebook.com/' + url;
+
+def extract_qualification(url):
+    req = requests.get(make_full_facebook_job_url(url))
+
+    soup = BeautifulSoup(req.text, 'lxml')
+
+    qualification = dict()
+    qualification['minimum'] = []
+    qualification['preferred'] = []
+    extract_qualification_details(soup, '_1n-_ _6hy- _8lf-', qualification, 'minimum', 'Minimum Qualifications')
+    extract_qualification_details(soup, '_1n-_ _6hy- _8lf-', qualification, 'preferred', 'Preferred Qualifications')
+    print(qualification)
+
+    return qualification
+
+def extract_qualification_details(soup, css_class, qualification, qualification_type, qual_type_description):
+    # Reviewing the DOM - one can isolate the DIV with the text "Minimum Qualifications"
+    # Then traversing the DOM two level up to isolate the LI tag and scraping the text within
+    # LI tag
+    min_qual_list = soup.find(text=re.compile(qual_type_description)).parent.parent.findAll('div', {'class' : css_class})
+
+    for min_qual in min_qual_list:
+        qualification[qualification_type].append(min_qual.text)
+
+
+#get_jobs()
+extract_qualification('/careers/jobs/2537073536607922/')
